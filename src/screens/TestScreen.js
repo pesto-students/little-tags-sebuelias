77ae552a1ab7fa69ca22f200a1d2871c5d905e11
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-08 17:54:28 +0530

    testing and error screens

---
+++ src/screens/TestScreen.js
@@ -0,0 +1,21 @@
+import logo from '../logo.svg';
+
+const TestScreen = () => (
+        <>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello Team Axios
+        </a>
+      </header>
+    </div>
+        </>
+    )
+
+export default TestScreen;
