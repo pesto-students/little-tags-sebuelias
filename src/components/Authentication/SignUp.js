acf3ae319593fdb411f368ea36c5177863a92006
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-15 18:04:49 +0530

    added facebook login feature

--- src/components/Authentication/SignUp.js
+++ src/components/Authentication/SignUp.js
@@ -1,22 +1,70 @@
+import {useContext, useState} from "react"
 import { FcGoogle } from 'react-icons/fc';
-import { FaFacebookF } from 'react-icons/fa'
-import Modal from "../Modal"
-import {  } from "./index.sass";
+import PropTypes from 'prop-types';
+import { FaFacebookF } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md'
+import Modal from '../Modal';
+import {} from './index.sass';
+import FirebaseContext from '../../services/Firebase/context';
 
-function SignUp() {
-    return (
-        <>
-        <div className="modal-sign">
-        <Modal>
+function SignUp({closeModal}) {
+
+  const firebase = useContext(FirebaseContext);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleGoogleSignIn = () => {
+    firebase
+      .doGoogleSignIn()
+      .then((authUser) => {
+        firebase.user(authUser.user.uid).set({
+          email: authUser.user.email,
+          username: authUser.user.displayName,
+        });
+        closeModal()
+      })
+      .catch((error) => setErrorMessage(error.message));
+  };
+
+  const handleFaceBookSignIn = () => {
+    firebase
+      .doFacebookSignIn()
+      .then((authUser) => {
+        firebase.user(authUser.user.uid).set({
+          email: authUser.user.email,
+          username: authUser.user.displayName,
+        });
+        closeModal()
+      })
+      .catch((error) => setErrorMessage(error.message));
+  };
+  
+  return (
+    <div className="modal-sign">
+      <Modal>
+        {!!errorMessage && <p className="error-message">{errorMessage}</p>}
+        <MdClose className="close-icon" onClick={()=>closeModal()}/>
         <div className="sign-up">
-            <h3>Log in / Sign up</h3>
-            <button className="sign-button" type="button"><FcGoogle /> Google Account</button>
-            <button className="sign-button" type="button"><FaFacebookF /> Facebook Account</button>
+          <h3>Log in / Sign up</h3>
+          <button
+            className="sign-button"
+            type="button"
+            onClick={handleGoogleSignIn}
+          >
+            <FcGoogle /> Google Account
+          </button>
+          <button className="sign-button" type="button"
+          onClick={handleFaceBookSignIn}
+          >
+            <FaFacebookF /> Facebook Account
+          </button>
         </div>
         </Modal>
-        </div>  
-        </>
+        </div> 
     )
+}
+
+SignUp.propTypes = {
+  closeModal: PropTypes.func.isRequired,
 }
 
 export default SignUp;
