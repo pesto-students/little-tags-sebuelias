ad1ed01c8031508d5153c34b6aa54cddcf7cd1c2
sebuelias <sebuelias27@gmail.com>
2021-04-15 21:11:11 +0530

    basic category in home page added

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import './index.scss';
+// import Men from '../../assets/categoryImages/men.jpg';
+// import Women from '../../assets/categoryImages/women.jpg';
+
+function Categories() {
+  return (
+    <div className="categories-container">
+      <div className="item category-1">
+        {/* <img src={Women} alt="women" className="image" /> */}
+      </div>
+      <div className="item category-2">
+        {/* <img src={Men} alt="men" className="image" /> */}
+      </div>
+      <div className="item category-3">category 3</div>
+      <div className="item category-4">category 4</div>
+      <div className="item category-5">category 5</div>
+    </div>
+  );
+}
+
+export default Categories;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect,useContext } from 'react';
+import { useState, useRef, useEffect, useContext } from 'react';
 import { FaRegHeart } from 'react-icons/fa';
 import { FiShoppingBag } from 'react-icons/fi';
 import PropTypes from 'prop-types';
@@ -7,7 +7,7 @@ import { useOnClickOutside } from '../../utils/hooks/useOnClickOutside';
 import FirebaseContext from '../../services/Firebase/context';
 import Burger from '../Burger';
 import Menu from '../Menu';
-import Signup from "../Authentication/SignUp"
+import Signup from '../Authentication/SignUp';
 import Search from '../Search';
 import './index.scss';
 
@@ -15,8 +15,34 @@ function Header(props) {
   const firebase = useContext(FirebaseContext);
   const [menuOpen, setMenuOpen] = useState(false);
   const [openModal, setopenModal] = useState(false);
+  // const [scroll, setScroll] = useState('header');
+
   const menuRef = useRef();
   useOnClickOutside(menuRef, () => setMenuOpen(false));
+
+  // let headerClassName = 'header';
+
+  // useEffect(() => {
+  //   const onScroll = () => {
+  //     // console.log(e);
+  //     console.log(window.scrollY);
+  //     if (window.scrollY > 50) {
+  //       // headerClassName = 'header-scrolled';
+  //       setScroll('header-scrolled');
+
+  //       // console.log(headerClassName);
+  //     } else {
+  //       // headerClassName = 'header';
+  //       setScroll('header');
+  //       // console.log(headerClassName);
+  //     }
+  //     // setScroll(window.screenY);
+  //   };
+
+  //   window.addEventListener('scroll', onScroll);
+
+  //   return () => window.removeEventListener('scroll', onScroll);
+  // }, [scroll]);
 
   const handleLoginModal = (event) => {
     event.preventDefault();
@@ -25,12 +51,12 @@ function Header(props) {
 
   const handleLogout = (event) => {
     event.preventDefault();
-    firebase.doSignOut()
-  }
+    firebase.doSignOut();
+  };
 
-  useEffect(()=>{
-    console.log("check props man", props)
-  })
+  useEffect(() => {
+    console.log('check props man', props);
+  });
 
   return (
     <>
@@ -48,30 +74,33 @@ function Header(props) {
 
         <div className="header-buttons">
           <ul>
-            { !props.authUser ? <li onClick={handleLoginModal} aria-hidden="true">
-              <a
-                className="login"
-                href="login.html"
-                data-toggle="tooltip"
-                data-selector="true"
-                data-placement="bottom"
-                title="Login / Register"
-              >
-                Login
-              </a>
-            </li> :
-            <li onClick={handleLogout} aria-hidden="true">
-              <a
-                className="login"
-                href="login.html"
-                data-toggle="tooltip"
-                data-selector="true"
-                data-placement="bottom"
-                title="Logout"
-              >
-                Logout
-              </a>
-            </li>}
+            {!props.authUser ? (
+              <li onClick={handleLoginModal} aria-hidden="true">
+                <a
+                  className="login"
+                  href="login.html"
+                  data-toggle="tooltip"
+                  data-selector="true"
+                  data-placement="bottom"
+                  title="Login / Register"
+                >
+                  Login
+                </a>
+              </li>
+            ) : (
+              <li onClick={handleLogout} aria-hidden="true">
+                <a
+                  className="login"
+                  href="login.html"
+                  data-toggle="tooltip"
+                  data-selector="true"
+                  data-placement="bottom"
+                  title="Logout"
+                >
+                  Logout
+                </a>
+              </li>
+            )}
           </ul>
 
           <ul>
@@ -89,7 +118,13 @@ function Header(props) {
           </ul>
         </div>
       </nav>
-      {openModal ? <Signup closeModal={() => {setopenModal(false)}}/> : null}
+      {openModal ? (
+        <Signup
+          closeModal={() => {
+            setopenModal(false);
+          }}
+        />
+      ) : null}
     </>
   );
 }
@@ -101,14 +136,14 @@ Header.propTypes = {
     emailVerified: PropTypes.bool,
     username: PropTypes.string,
   }),
-}
+};
 
 Header.defaultProps = {
-  authUser: null
-}
+  authUser: null,
+};
 
 const mapStateToProps = (state) => ({
-  authUser: state.authDetails.auth
+  authUser: state.authDetails.auth,
 });
 
 export default connect(mapStateToProps)(Header);
