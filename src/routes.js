ed26558064c0665e76988982d9109c1d26cb6177
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-15 18:08:51 +0530

    added header and footer in parent as they will be on every screen

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,35 @@
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './utils/constants/routePaths';
-// import { Home, /* ErrorPage, */ Category } from './screens';
 
 import ErrorPage from './screens/ErrorPage';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Home from './screens/Home';
 import Category from './screens/Category';
 
+
 function Routes() {
   return (
+    <>
+<Header />
+    <BrowserRouter>
     <Switch>
-      <Route exact path={ROUTES.HOME}>
+    <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
       <Route path={ROUTES.CATEGORIES}>
+        <Category />
+      </Route>
+      <Route path={ROUTES.SIGN_IN}>
         <Category />
       </Route>
       <Route path={ROUTES.ERROR}>
         <ErrorPage />
       </Route>
     </Switch>
+  </BrowserRouter>
+  <Footer />
+  </>
   );
 }
 
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,16 +1,13 @@
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+
 
 const Home = () => {
   console.log('HOME');
   return (
     <div>
       <div>
-        <Header />
         <div
           style={{ height: '700px', width: '100%', backgroundColor: 'wheat' }}
         />
-        <Footer />
       </div>
     </div>
   );
