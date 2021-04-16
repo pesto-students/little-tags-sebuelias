8214713d1db2224514fd41ffad2cf978c23b0c10
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-16 19:22:02 +0530

    category page added

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,22 +1,21 @@
-// import React from 'react'
+import PropTypes from 'prop-types';
 import './index.scss';
-// import Men from '../../assets/categoryImages/men.jpg';
-// import Women from '../../assets/categoryImages/women.jpg';
 
-function Categories() {
+function Categories(props) {
   return (
     <div className="categories-container">
-      <div className="item category-1">
-        {/* <img src={Women} alt="women" className="image" /> */}
-      </div>
-      <div className="item category-2">
-        {/* <img src={Men} alt="men" className="image" /> */}
-      </div>
-      <div className="item category-3">category 3</div>
-      <div className="item category-4">category 4</div>
-      <div className="item category-5">category 5</div>
+      <div className="item category-1" onClick={() => {props.history.push({pathname: '/categories', state: { QueryCategory: "women clothing"}})}} aria-hidden="true">WOMENS CLOTHING</div>
+      <div className="item category-2" onClick={() => {props.history.push({pathname: '/categories', state: { QueryCategory: "men clothing"}})}} aria-hidden="true">MENS CLOTHING</div>
+      <div className="item category-3" onClick={() => {props.history.push({pathname: '/categories', state: { QueryCategory: "electronics"}})}} aria-hidden="true">ELECTRONICS</div>
+      <div className="item category-4" onClick={() => {props.history.push({pathname: '/categories', state: { QueryCategory: "jewelery"}})}} aria-hidden="true">JWELLERY</div>
+      <div className="item category-5" onClick={() => {props.history.push({pathname: '/categories', state: { QueryCategory: "all-products"}})}} aria-hidden="true">ALL PRODUCTS</div>
     </div>
   );
 }
+
+Categories.propTypes = {
+  history: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
 
 export default Categories;
