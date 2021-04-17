947c2987885a90652b8b595e8b65dccbda21b4b4
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-17 20:39:35 +0530

    added routing for product page and whislist

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types';
 import "./index.sass"
+import AddRemoveWhislist from '../AddRemoveWhislist';
 
-const Card = (props) => (
+const Card = ({ index, value, history }) => (
   <>
-    <div className="individual-content-box" key={props.index.toString()}>
-    <img className="image-box" src={props.value.image} alt="test" />
-    <h3 className="title-head">{props.value.title}</h3>
-    <h3>&#8377; {props.value.price}</h3>
+    <div className="individual-content-box" key={index.toString()}>
+      <div className="whislist-div"><AddRemoveWhislist productDetail={value}/></div>
+    <img className="image-box" src={value.image} alt={value.title}  aria-hidden="true" onClick={() => {history.push({pathname: `/categories/${value.id}`, state: {product :value}})}}/>
+    <h3 className="title-head">{value.title}</h3>
+    <h3>&#8377; {value.price}</h3>
     </div>
     </>
-)
+  )
 
 Card.propTypes = {
     index: PropTypes.number.isRequired,
     value: PropTypes.objectOf(PropTypes.string).isRequired,
+    history: PropTypes.objectOf(PropTypes.object).isRequired,
   };
 
 export default Card;
