56bacb352d4439645bceaecc11c054ca0b153578
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-17 20:37:39 +0530

    added whislist object storage

--- src/store/modules/apparrelData/sagas.js
+++ src/store/modules/apparrelData/sagas.js
@@ -1,22 +1,33 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { storeData, storeError } from "./actions";
-import { REQUEST_API_DATA } from '../types'
+import { storeData, storeError, addWhislist, removeWhislist } from "./actions";
+import { REQUEST_API_DATA, HIT_WHISLIST } from '../types'
 import getApparrelData from "./api";
 
 function* getData(action) {
     try {
         const data = yield call(getApparrelData, action.payload);
-        if (data.data.error) {
-            yield put(storeError({error: data.error}));
-        } else {
-            yield put(storeData({apparrelData:data.data}));
+        yield put(storeData({apparrelData:data}));
+    } catch (e) {
+        yield put(storeError({error: e.message}));
+    }
+}
+
+function* whislistData(action) {
+    try {
+        if (action.payload.actionType === "add") {
+            yield put(addWhislist(action.payload.productDetail))
+        } else if (action.payload.actionType === "remove") {
+            yield put(removeWhislist(action.payload.productDetail))
         }
     } catch (e) {
         yield put(storeError({error: e.message}));
     }
 }
 
-
-export default function* watchApparrelData() {
+export function* watchApparrelData() {
     yield takeEvery(REQUEST_API_DATA, getData);
+}
+
+export function* watchWhislistData() {
+    yield takeEvery(HIT_WHISLIST, whislistData);
 }
