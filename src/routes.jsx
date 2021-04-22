91a23845d2f93cbd6ce856bd3cdea42299d446f0
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-22 18:40:57 +0530

    added routes for address and order placed

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,8 @@ import ErrorPage from './screens/ErrorPage';
 import Home from './screens/Home';
 import Category from './screens/Category';
 import Product from './screens/Product'
+import Address from './screens/Address';
+import OrderPlaced from './screens/OrderPlaced';
 
 function Routes() {
   return (
@@ -15,6 +17,8 @@ function Routes() {
           <Route exact path={ROUTES.HOME} component={Home}/>
           <Route exact path={ROUTES.CATEGORIES} component={Category}/>
           <Route exact path={ROUTES.PRODUCT} component={Product}/>
+          <Route exact path={ROUTES.ADDRESS} component={Address} />
+          <Route exact path={ROUTES.ORDER_PLACED} component={OrderPlaced} />
           <Route exact path={ROUTES.SIGN_IN} component={Category} />
           <Route path={ROUTES.ERROR} component={ErrorPage} />
         </Switch>
