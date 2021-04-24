9f475dd15dad17aedb6895be597d6cc02cf3f953
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-24 09:39:09 +0530

    added order placed sceen

--- src/screens/OrderPlaced/index.jsx
+++ src/screens/OrderPlaced/index.jsx
@@ -1,31 +1,49 @@
 import PropTypes from 'prop-types';
+import { useEffect, useContext } from 'react';
 import { connect } from 'react-redux';
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
+import FirebaseContext from '../../services/Firebase/context';
 import './index.scss';
 
-const OrderPlaced = (props) => (
-  <>
-    <Header />
-    <div style={{ height: '100px' }} />
-    <div className="order-placed">
-      <h1>Order placed!!!</h1>
-      <p>Thank you for choosing us.</p>
-      <button
-        type="button"
-        onClick={() => {
-          props.history.push({ pathname: '/category' });
-        }}
-      >
-        Continue Shopping
-      </button>
-    </div>
-    <Footer />
-  </>
-);
+const OrderPlaced = (props) => {
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(() => {
+    if (props.location.state.previousLocation === 'payment') {
+      firebase.saveDataToDatabase(
+        props.authDetails.uid,
+        'order',
+        props.apparrelData.order
+      );
+      firebase.saveDataToDatabase(props.authDetails.uid, 'cart', []);
+    } else {
+      props.history.push({ pathname: '/categories' });
+    }
+  }, []);
+
+  return (
+    <>
+      <div style={{ height: '100px' }} />
+      <div className="order-placed">
+        <h1>Order placed!!!</h1>
+        <p>Thank you for choosing us.</p>
+        <button
+          type="button"
+          onClick={() => {
+            props.history.push({ pathname: '/categories' });
+          }}
+        >
+          Continue Shopping
+        </button>
+      </div>
+    </>
+  );
+};
 
 OrderPlaced.propTypes = {
   history: PropTypes.objectOf(PropTypes.object).isRequired,
+  authDetails: PropTypes.objectOf(PropTypes.object).isRequired,
+  apparrelData: PropTypes.objectOf(PropTypes.object).isRequired,
+  location: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
 const mapStateToProps = (state) => ({
