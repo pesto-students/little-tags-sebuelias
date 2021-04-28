32326e8fb3e7b9ca43ba807ef27421f836e04ad1
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-28 23:15:03 +0530

    moved sort to filter section

--- src/components/Sort/index.jsx
+++ src/components/Sort/index.jsx
@@ -1,67 +1,37 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './index.sass';
 
 function Sort(props) {
-  const [openDropDown, setopenDropDown] = useState(false);
-  const [sortedValue, setsortedValue] = useState('SORT: NONE');
 
   const handleDropDownValue = (event) => {
-    setsortedValue(event.target.value);
-    setopenDropDown(!openDropDown);
     props.handleDropDownValue(event);
   };
 
   return (
     <div
       className="drop-down-div"
-      onMouseLeave={(event) => {
-        event.preventDefault();
-        setopenDropDown(false);
-      }}
     >
-      <input
-        id="level"
-        name="level"
-        type="button"
-        onClick={() => setopenDropDown(!openDropDown)}
-        onMouseOver={(event) => {
-          event.preventDefault();
-          setopenDropDown(true);
-        }}
-        onFocus={(event) => {
-          event.preventDefault();
-          setopenDropDown(false);
-        }}
-        className="dropdown general-font-design"
-        value={sortedValue}
-      />
-
-      {openDropDown ? (
-        <div className="dropdown-menu">
           <input
             type="button"
             className="dropdown-content general-font-design"
-            value="SORT: NONE"
+            value="NONE"
             name={1}
             onClick={handleDropDownValue}
           />
           <input
             type="button"
             className="dropdown-content general-font-design"
-            value="SORT: HIGH TO LOW"
+            value="HIGH TO LOW"
             name={1}
             onClick={handleDropDownValue}
           />
           <input
             type="button"
             className="dropdown-content general-font-design"
-            value="SORT: LOW TO HIGH"
+            value="LOW TO HIGH"
             name={1.5}
             onClick={handleDropDownValue}
           />
-        </div>
-      ) : null}
     </div>
   );
 }
