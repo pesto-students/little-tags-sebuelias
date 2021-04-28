12514e09ee8f3b377c6a5fd9abad9ca24b768b0e
prateekeshwar <prateekeshwar@paymeindia.in>
2021-04-28 12:37:48 +0530

    added past order page

--- src/screens/PastOrder/index.jsx
+++ src/screens/PastOrder/index.jsx
@@ -1,41 +1,71 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './index.scss';
 
 const PastOrder = (props) => {
-    const test = new Date(props.apparrelData.order[0].orderDate)
-console.log(props, "aaaaaaaaaaaaaaaaaaaaaaaa", props.apparrelData, test)
-return(
+  const convertDate = (inputFormat) => {
+    function pad(value) {
+      return value < 10 ? `0${value}` : value;
+    }
+    const date = new Date(inputFormat);
+    return [
+      pad(date.getDate()),
+      pad(date.getMonth() + 1),
+      date.getFullYear(),
+    ].join('-');
+  };
+
+  const visualizeOrder = (props.apparrelData.order || []).map(
+    (value, index) => (
+      <div className="flex-column order-box" key={index.toString()}>
+        <div className="flex-row order-id">
+          <div>order date: {convertDate(value.orderDate)}</div>
+          <div>payment id: {value.paymentId}</div>
+        </div>
+        <div>
+          {value.cart.map((cartValue, cartIndex) => (
+            <div className="flex-row cart-row" key={cartIndex.toString()}>
+              <img
+                className="image-box"
+                src={cartValue.image}
+                alt={cartValue.title}
+                aria-hidden="true"
+                onClick={() => {}}
+              />
+              <div className="flex-column cart-title">
+                <p className="title-head">{cartValue.title}</p>
+                <p className="size-info">
+                  {cartValue.size ? `SIZE: ${cartValue.size}` : null}
+                </p>
+              </div>
+              <div className="flex-column quantity-price">
+                <p>Quantity:{cartValue.quantity}</p>
+              </div>
+              <p className="quantity-price">
+                &#8377; {(cartValue.price * cartValue.quantity).toFixed(2)}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="flex-column">
+          <h3 className="address-order">{value.address.name}</h3>
+          <p>{value.address.address}</p>
+        </div>
+      </div>
+    )
+  );
+
+  return (
     <>
-    <div style={{ height: '100px' }} />
-    <div className="flex-column">
-        <div className="flex-row">
-            <div>
-                order date: {test.getDay()} {test.getMonth()}
-            </div>
-            <div>
-                payment id
-            </div>
-            </div>
-            <div>
-                our body
-            </div>
-            <div>
-                address man 
-            </div>
-    </div>
+      <div style={{ height: '100px' }} />
+      <div>{visualizeOrder}</div>
     </>
-)
-}
+  );
+};
 
 PastOrder.propTypes = {
-    // requestData: PropTypes.func.isRequired,
-    // QueryCategory: PropTypes.string.isRequired,
-    // changeSignUpBool: PropTypes.func.isRequired,
-    // location: PropTypes.objectOf(PropTypes.object).isRequired,
-    // history: PropTypes.objectOf(PropTypes.object).isRequired,
-    apparrelData: PropTypes.arrayOf(PropTypes.object).isRequired,
-  };
-// const dispatchToProps = { requestData, changeSignUpBool };
+  apparrelData: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   apparrelData: state.apparrelData,
